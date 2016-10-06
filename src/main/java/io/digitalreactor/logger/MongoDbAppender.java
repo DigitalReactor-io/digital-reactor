6c3c9dd122b00636aac085a560601aa0fab35873
ingvard <maxim.stepahcev@gmail.com>
2016-10-06 17:26:10 +0300

    MongoDb use old version driver 1.2

--- src/main/java/io/digitalreactor/logger/MongoDbAppender.java
+++ src/main/java/io/digitalreactor/logger/MongoDbAppender.java
@@ -2,33 +2,29 @@ package io.digitalreactor.logger;
 
 import ch.qos.logback.classic.spi.LoggingEvent;
 import ch.qos.logback.core.AppenderBase;
-import ch.qos.logback.core.db.dialect.SybaseSqlAnywhereDialect;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
-import com.mongodb.WriteConcern;
-import com.mongodb.client.MongoDatabase;
-import org.apache.log4j.Level;
-import org.bson.Document;
-
-import java.net.UnknownHostException;
 
 /**
  * Created by MStepachev on 06.10.2016.
  */
 public class MongoDbAppender extends AppenderBase<LoggingEvent> {
 
-    private final MongoClient mongoClient;
+    /*private final MongoClient mongoClient;
     private final MongoDatabase mongoDatabase;
     private final String collectionName;
     private final String databaseName;
-
-    public MongoDbAppender() throws UnknownHostException {
+*/
+   /* public MongoDbAppender() throws UnknownHostException {
         this.mongoClient = new MongoClient(System.getenv("mongodb_host"));
         this.collectionName = System.getenv("mongodb_logs_collection");
         this.databaseName = System.getenv("mongodb_db");
         this.mongoDatabase = this.mongoClient.getDatabase(databaseName);
-    }
+    }*/
 
-    @Override
+   /* @Override
     public void start() {
         super.start();
         //TODO[St.maxim] remove constructor
@@ -40,17 +36,17 @@ public class MongoDbAppender extends AppenderBase<LoggingEvent> {
         if (mongoClient != null) {
             mongoClient.close();
         }
-    }
+    }*/
 
     @Override
     protected void append(LoggingEvent eventObject) {
 
-        Document log = new Document();
-        log.append("level", eventObject.getLevel());
-        log.append("timeStamp", eventObject.getTimeStamp());
-        log.append("loggerName", eventObject.getLoggerName());
-        log.append("message", eventObject.getMessage());
-        log.append("threadName", eventObject.getThreadName());
+        DBObject log =  new BasicDBObject();
+        log.put("level", eventObject.getLevel().toString());
+        log.put("timeStamp", eventObject.getTimeStamp());
+        log.put("loggerName", eventObject.getLoggerName());
+        log.put("message", eventObject.getMessage());
+        log.put("threadName", eventObject.getThreadName());
         // log.append("argumentArray", eventObject.getArgumentArray());
         //  log.append("marker", eventObject.getMarker().toString());
         // log.append("stackTrace", eventObject.getThrowableProxy().getStackTraceElementProxyArray());
@@ -59,17 +55,20 @@ public class MongoDbAppender extends AppenderBase<LoggingEvent> {
 
         synchronized (this) {
 
+            //TODO try with resource
             try {
-                //mongoDatabase.getCollection(collectionName).insertOne(new Document());
-
                 MongoClient md = new MongoClient();
-                MongoDatabase testdb = md.getDatabase("test");
-                //, WriteConcern.SAFE
-                testdb.getCollection("logs").insertOne(new Document());
+
+                //mongoDatabase.getCollection(collectionName).insertOne(new Document());
+                DB test = md.getDB("test");
+                test.getCollection("logs").insert(log);
+
 
                 System.out.println("Mongo fine: " + eventObject.getMessage());
+                md.close();
             } catch (Exception e) {
                 e.printStackTrace();
+
             }
         }
 
