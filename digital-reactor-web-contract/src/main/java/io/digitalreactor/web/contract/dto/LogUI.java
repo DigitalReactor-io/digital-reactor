be493bf2f84e895a685ad02b364fd25ae9b9f37e
ingvard <maxim.stepahcev@gmail.com>
2016-10-07 18:06:16 +0300

    Draft data

--- digital-reactor-web-contract/src/main/java/io/digitalreactor/web/contract/dto/LogUI.java
+++ digital-reactor-web-contract/src/main/java/io/digitalreactor/web/contract/dto/LogUI.java
@@ -4,4 +4,37 @@ package io.digitalreactor.web.contract.dto;
  * Created by MStepachev on 07.10.2016.
  */
 public class LogUI {
+    private String level;
+    private String timeStamp;
+    private String loggerName;
+    private String message;
+    private String threadName;
+
+    public LogUI(String level, String timeStamp, String loggerName, String message, String threadName) {
+        this.level = level;
+        this.timeStamp = timeStamp;
+        this.loggerName = loggerName;
+        this.message = message;
+        this.threadName = threadName;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+
+    public String getLoggerName() {
+        return loggerName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getThreadName() {
+        return threadName;
+    }
 }
